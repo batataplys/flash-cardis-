98908cbdd889fb68701664220f14d12a2ca9dff5
batataplys <163124843+batataplys@users.noreply.github.com>
2024-10-24 18:33:28 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação 2.0',
+    'O que é python?',
+    'O python é uma linguagem de programação'
+  )
+  criaCartao(
+    'Gatos',
+    'O que é um gato?',
+    'O gato é um mamífero carnívoro da família dos felídeos, muito popular como animal de estimação'
+  )
+  criaCartao(
+    'HTML',
+    'O que é HTML?',
+    'O HTML é uma linguagem de marcação de hipertexto'
+  )
+  criaCartao(
+    'Astronomia',
+    'O que é a astronomia?',
+    'A astronomia é a ciência que estuda os corpos celestes (planetas, asteroides, cometas, estrelas, galáxias, etc.)'
+  )
+  criaCartao(
+    'Olho',
+    'Qual a função do olho?',
+    'O olho ajusta constantemente a quantidade de luz que deixa entrar, e gera imagens contínuas que são transmitidas ao cérebro de forma instantânea.'
+  )
+  criaCartao(
+    'Coração',
+    'Qual a função do coraçao?',
+    'A função do coração é bombear o sangue para todo o corpo.'
+  )
+  criaCartao(
+    'Mitocondria',
+    'Para que serve a Mitocondria?',
+    'As mitocôndrias funcionam como um sítio do processo de respiração celular'
+  )
